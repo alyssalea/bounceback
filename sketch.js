5468853deb16a4678b70141b045bfe7457082cca
alyssalea <alyssalea@icloud.com>
2019-04-18 02:38:23 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var x = 0
+var speed 
+var w = 1
+
+function setup() {
+  createCanvas(600,600);
+  //background(255,0,255);
+}
+
+function draw() {
+   background(255,0,255)      
+   fill(random (1,255));
+  
+  if(x > 600 - w/2){
+ 
+ speed = speed - 3;//or speed =- 3
+  }
+  
+   if(x < 0 + w/2 ){
+  speed = speed =+ 3;//or speed =+ 3
+  }
+  
+  ellipse(x, 300, w)
+  
+  x = x + speed
+  w = w + 1
+ 
+}
+
+
+
+
+ // try x = 1 after if x > 600
